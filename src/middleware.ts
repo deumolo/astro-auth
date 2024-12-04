524c0044e36fcbc65c22ab227ee7bb222774867e
Moreno, Daniel <deumolo@gmail.com>
2024-12-04 13:12:36 -0600

    Se limpió el archivo middleware que estaba lleno de codigo no usado

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,26 +5,5 @@ const privateRoutes = ['/protected']
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
-
-    const authHeaders = context.request.headers.get('authorization') ?? '';
-
-    if (privateRoutes.includes(context.url.pathname)) {
-        return checkLocalAuth(authHeaders, next);
-    }
-
     return next();
 });
-
-const checkLocalAuth = (authHeaders: string, next: MiddlewareNext) => {
-    if (authHeaders) {
-        const authValue = authHeaders.split(' ')[1] ?? 'user:pass';
-        const decodedValue = atob(authValue).split(':');
-        const [user, password] = decodedValue;
-
-        if (user === 'admin' || password === 'admin') {
-            return next();
-        }
-    }
-
-    return new Response('Unauthorized', { status: 401, headers: { 'WWW-Authenticate': 'Basic real="Secure Area"' } });
-}
