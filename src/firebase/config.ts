09e8065190abec50d3878ec5e623085648908264
Moreno, Daniel <deumolo@gmail.com>
2024-12-04 11:54:01 -0600

    Se instaló firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMw3d2mK8yApzOLbcFrgrMnxNogGUYJuI",
+  authDomain: "astro-auth-7f003.firebaseapp.com",
+  projectId: "astro-auth-7f003",
+  storageBucket: "astro-auth-7f003.firebasestorage.app",
+  messagingSenderId: "316027801014",
+  appId: "1:316027801014:web:8956fcc68fe690118f0517"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
