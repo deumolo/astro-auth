2c79c1d9ad6c3153f6c64d6552b060072a7de689
Moreno, Daniel <deumolo@gmail.com>
2024-12-04 11:06:52 -0600

    Se creo un ejemplo de un check de auth local para rutas protegidas, puro demonstrativo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
 
 const privateRoutes = ['/protected']
@@ -5,14 +6,25 @@ const privateRoutes = ['/protected']
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
 
-    const authHeaders = context.request.headers.get('authorization');
+    const authHeaders = context.request.headers.get('authorization') ?? '';
 
     if (privateRoutes.includes(context.url.pathname)) {
-        if (authHeaders) {
-            return next();
-        }
-        return new Response('Unauthorized', { status: 401, headers: { 'WWW-Authenticate': 'Basic real="Secure Area"' } });
+        return checkLocalAuth(authHeaders, next);
     }
 
     return next();
 });
+
+const checkLocalAuth = (authHeaders: string, next: MiddlewareNext) => {
+    if (authHeaders) {
+        const authValue = authHeaders.split(' ')[1] ?? 'user:pass';
+        const decodedValue = atob(authValue).split(':');
+        const [user, password] = decodedValue;
+
+        if (user === 'admin' || password === 'admin') {
+            return next();
+        }
+    }
+
+    return new Response('Unauthorized', { status: 401, headers: { 'WWW-Authenticate': 'Basic real="Secure Area"' } });
+}
