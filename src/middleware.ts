62f45724fc2874f023c3ebf87d7e4383fdc7085c
Moreno, Daniel <deumolo@gmail.com>
2024-12-04 23:09:50 -0600

    Se declararon variables locales disponibles en los diferentes componentes y páginas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
+import { firebase } from './firebase/config';
 
 const privateRoutes = ['/protected']
 
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware((context, next) => {
+export const onRequest = defineMiddleware(({ url, request, locals }, next) => {
+    const isLoggedIn = !!firebase.auth.currentUser;
+    const user = firebase.auth.currentUser;
+
+    locals.isLoggedIn = isLoggedIn;
+
     return next();
 });
