a89bd3fde3d21bab51b9541ef737aff597135452
Moreno, Daniel <deumolo@gmail.com>
2024-12-04 23:24:00 -0600

    Se agregó el dato user a las locals

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,15 @@ export const onRequest = defineMiddleware(({ url, request, locals }, next) => {
     const isLoggedIn = !!firebase.auth.currentUser;
     const user = firebase.auth.currentUser;
 
+    if (user) {
+        locals.user = {
+            email: user.email ?? '',
+            name: user.displayName ?? '',
+            avatar: user.photoURL ?? '',
+            emailVerified: user.emailVerified
+        }
+    }
+
     locals.isLoggedIn = isLoggedIn;
 
     return next();
