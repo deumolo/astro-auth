06de58323a77cb17911e5f6d943022b4173abe6d
Moreno, Daniel <deumolo@gmail.com>
2024-12-07 09:22:18 -0600

    Aquí no pasó nada 😂

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify({
+    imageCDN: false,
+  }),
 });
